75d4181ad61f38f5da4e320468f8e791314b170f
MathSena <MathSena07@hotmail.com>
2023-10-28 19:54:34 +0100

    Create the ThemeToogle

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import { Footer } from '@/components/footer/Footer'
 import { ThemeContextProvider } from '@/context/ThemeContext'
-import ThemeProvider from '@/providers/ThemeProvider'
+import { ThemeProvider } from '@/providers/ThemeProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,13 +17,15 @@ export default function RootLayout({ children }) {
     <html lang="pt-BR">
       <body className={inter.className}>
         <ThemeContextProvider>
-          <div className="container">
-            <div className="wrapper">
-              <Navbar />
-              {children}
-              <Footer />
+          <ThemeProvider>
+            <div className="container">
+              <div className="wrapper">
+                <Navbar />
+                {children}
+                <Footer />
+              </div>
             </div>
-          </div>
+          </ThemeProvider>
         </ThemeContextProvider>
       </body>
     </html>
