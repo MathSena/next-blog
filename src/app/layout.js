ed8a5378c973173bb78bf0f9f03ed296716e703c
MathSena <MathSena07@hotmail.com>
2023-10-28 18:44:37 +0100

    CSS Responsive Layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,14 +4,17 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Blog App',
-  description: 'The best blog app!',
+  title: 'mathsena.dev',
+  description: 'Tech, lifestyle, travel and photography!'
 }
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-BR">
+      <body className={inter.className}>
+        <div className="container">{children}</div>
+        <div className="footer">{children}</div>
+      </body>
     </html>
   )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,8 @@
+import Link from "next/link";
 import styles from "./homepage.module.css";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return <div>
+    <Link href="/blog">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis, nobis deserunt porro, totam inventore tempore, incidunt qui aliquam cum commodi unde! Repudiandae adipisci fuga cupiditate velit, soluta corrupti rerum commodi.</Link>
+  </div>;
 }
